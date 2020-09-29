23d1ea4d719e83d8af3954d276cabe85a1d47884
Christopher Demahy <demahyc@gmail.com>
2020-09-29 18:35:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  data = YAML.load_file(path)
+  data.collect do |key, value|
+    data[key] = { english: value[0], japanese: value[1] }
+  end
+  data
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  data = load_library(path)
+  data.each do |key, value|
+    if value[:english] == emoticon
+      value[:japanese]
+    end
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
+  data = load_library(path)
+  data.each do |key, value|
+    if value[:japanese] == emoticon
+      binding.pry
+      key
+    end
+  end
 end
