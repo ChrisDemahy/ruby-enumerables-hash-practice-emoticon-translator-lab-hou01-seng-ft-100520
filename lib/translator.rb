cb924991c94e4802603a081ede3156a90ad47952
Christopher Demahy <demahyc@gmail.com>
2020-09-29 18:57:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,29 +2,39 @@
 require "yaml"
 require 'pry'
 
+# Loads the emoticon YAML file using the yaml gem
+# Accepts the relative path of the file
 def load_library(path)
   data = YAML.load_file(path)
-  data.collect do |key, value|
+  # maps the data to their proper language despcriptions
+  data.map do |key, value|
     data[key] = { english: value[0], japanese: value[1] }
   end
   data
 end
 
+# Get the japenese emoticon for the given english emoticon and the file path
 def get_japanese_emoticon(path, emoticon)
+  # load the data into a hash
   data = load_library(path)
+  # Loop through all the data
   data.each do |key, value|
+    #if there is a match for the english emoticon then return the japanese variant 
     if value[:english] == emoticon
       return value[:japanese]
     end
   end
+  # if the emoticon is not found a sorry message is returned 
   return "Sorry, that emoticon was not found"
 end
 
+# Get the english name or meaning for a japenese emoticon 
 def get_english_meaning(path, emoticon)
-  # code goes here
+  # Load the data 
   data = load_library(path)
+  # Loops across the data to see if it matches any japenese or english emoticon
   data.each do |key, value|
-    if value[:japanese] == emoticon
+    if value[:japanese] == emoticon || value[:english] == emoticon
       #binding.pry
       return key
     end
